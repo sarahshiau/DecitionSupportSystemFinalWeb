d360410516a6b9f339c13f2559077d6a08d4f07f
Sarah <sarah.shiau@gmail.com>
2021-12-12 01:18:31 +0800

    1.網頁完成 2.前端檢驗還未完成 3.前端資料傳送完成 4.後端資料接收完成

---
+++ index.js
@@ -0,0 +1,81 @@
+//1.按下submit後再去執行
+document.getElementById("form-buttom").addEventListener("click", function(){
+    //取值前先檢查是否有漏填問卷(value==null)
+    function chk(){
+        if(!getElementById(q1-degreeradio-5)){
+            console.log(1);
+            alert('問卷未填寫完!');
+        }
+        // if(que1==''){
+        //     console.log(1)
+        //     alert('問卷未填寫完!');
+        //     return false;
+        // }
+        //     return true;
+      }
+    alert('hello');
+    //2.取得每題被選取的value
+    selectage = document.querySelector('#age');
+    var age = selectage.value;
+    selectjob = document.querySelector('#job');
+    var job = selectjob.value;
+    // var que1 = document.querySelector('input[name="q1"]:checked').value;
+    // var que2 = document.querySelector('input[name="q2"]:checked').value;
+    // var que3 = document.querySelector('input[name="q3"]:checked').value;
+    // var que4 = document.querySelector('input[name="q4"]:checked').value;
+    // var que5 = document.querySelector('input[name="q5"]:checked').value;
+    // var que6 = document.querySelector('input[name="q6"]:checked').value;
+    // var que7 = document.querySelector('input[name="q7"]:checked').value;
+    // var que8 = document.querySelector('input[name="q8"]:checked').value;
+    // var que9 = document.querySelector('input[name="q9"]:checked').value;
+    // var que10 = document.querySelector('input[name="q10"]:checked').value;
+    
+
+	//3.Javascript轉成JSON
+    let data = {
+        age,
+        job,
+        // que1,
+        // que2,
+        // que3,
+        // que4,
+        // que5,
+        // que6,
+        // que7,
+        // que8,
+        // que9,
+        // que10
+    }
+    
+    console.log(data)
+
+	//4.傳送
+    axios.post('/user', data).then(resp => {
+
+    })
+    .catch(error => {
+        let resp = {
+            "result1": {
+              "ent": "cht",
+              "solution": "499"
+            },
+            "result2": {
+              "ent": "cht",
+              "solution": "499"
+            },
+            "result3": {
+              "ent": "cht",
+              "solution": "499"
+            }
+          };
+
+          console.log(resp)
+    });
+
+    //test
+    // console.log(que1);
+
+
+    //跳轉到結果畫面
+    // window.location.href="./result.html"
+});
