004865643c0ef0a2666c0cd5a066b35b08fd5210
Sarah <sarah.shiau@gmail.com>
2021-12-15 12:36:53 +0800

    前端檢驗完成

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
-//1.按下submit後再去執行
+///////////1.按下submit後再去執行
 document.getElementById("form-buttom").addEventListener("click", function(){
     //取值前先檢查是否有漏填問卷(value==null)
-    // function chk(){
-    //     if(!getElementById(q1-degreeradio-5=='null')){
-    //         console.log(1);
-    //         alert('問卷未填寫完!');
-    //     }
-        // if(que1==''){
-        //     console.log(1)
-        //     alert('問卷未填寫完!');
-        //     return false;
-        // }
-        //     return true;
-      // })
 
-    //2.取得每題被選取的value
+    console.clear()
+    console.log(document.querySelector('input[name="q1"]:checked'))
+
+    if (!document.querySelector('input[name="q1"]:checked')) {
+      alert('問卷尚未填寫完!');
+      return;
+    }
+    if (!document.querySelector('input[name="q2"]:checked')) {
+      alert('問卷尚未填寫完!');
+      return;
+    }
+    if (!document.querySelector('input[name="q3"]:checked')) {
+      alert('問卷問填寫完!');
+      return;
+    }
+    if (!document.querySelector('input[name="q4"]:checked')) {
+      alert('問卷尚未填寫完!');
+      return;
+    }
+    if (!document.querySelector('input[name="q5"]:checked')) {
+      alert('問卷尚未填寫完!');
+      return;
+    }
+    if (!document.querySelector('input[name="q6"]:checked')) {
+      alert('問卷尚未填寫完!');
+      return;
+    }
+    if (!document.querySelector('input[name="q7"]:checked')) {
+      alert('問卷尚未填寫完!');
+      return;
+    }
+    if (!document.querySelector('input[name="q8"]:checked')) {
+      alert('問卷尚未填寫完!');
+      return;
+    }
+    if (!document.querySelector('input[name="q9"]:checked')) {
+      alert('問卷尚未填寫完!');
+      return;
+    }
+    if (!document.querySelector('input[name="q10"]:checked')) {
+      alert('問卷尚未填寫完!');
+      return;
+    }
+    
+    //////////////2.取得每題被選取的value
     selectage = document.querySelector('#age');
     var age = selectage.value;
     selectjob = document.querySelector('#job');
@@ -32,13 +64,10 @@ document.getElementById("form-buttom").addEventListener("click", function(){
     var que10 = document.querySelector('input[name="q10"]:checked').value;
     
 
-	//3.Javascript轉成JSON
+////////////3.Javascript轉成JSON
     let data = {
-        age,
-        job,
-        que1,
-        que2,
-        que3,
+        age,job,
+        que1,que2,que3,
         que4,
         que5,
         que6,
@@ -50,8 +79,10 @@ document.getElementById("form-buttom").addEventListener("click", function(){
     
     console.log(data)
 
-	//4.傳送
-    axios.post('/user', data).then(resp => {
+////////////////4.傳送+接受回傳值
+    const url = 'http://10.10.10.100/getbrand.aspx';
+    
+    axios.post(url, data).then(resp => {
 
     })
     .catch(error => {
@@ -69,17 +100,11 @@ document.getElementById("form-buttom").addEventListener("click", function(){
               "solution": "499"
             }
           };
-
-          console.log(resp)
-          var strData=JSON.stringify(resp);
+          ////////////儲存資料
+          console.log(resp);
           localStorage.setItem("resp",strData); 
     });
 
-    //test
-    // console.log(que1);
-
-    // alert('窩還沒寫完ˊowoˋ');
-    // //跳轉到結果畫面
-
+    //////////////跳轉到結果畫面
     window.location.href = "./result.html";
 });
